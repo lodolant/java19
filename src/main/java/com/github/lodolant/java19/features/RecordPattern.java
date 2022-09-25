f64df168a900fc18b4fc9333dd4b22bdadb7fafe
Lionel Odolant <lionel.odolant@gmail.com>
2022-09-26 01:45:55 +0200

    Use Java 19 Record Pattern

--- src/main/java/com/github/lodolant/java19/features/RecordPattern.java
+++ src/main/java/com/github/lodolant/java19/features/RecordPattern.java
@@ -1,6 +1,8 @@
 package com.github.lodolant.java19.features;
 
 import com.github.lodolant.java19.model.Attack;
+import com.github.lodolant.java19.model.PokemonKind;
+import com.github.lodolant.java19.model.pokemon.Pokemon;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,8 +11,8 @@ public class RecordPattern {
 
     public static String makeAttack(Object o) {
         LOGGER.info("Making attack");
-        if (o instanceof Attack attack) {
-            return "Attack %s (%s) from %s, strength of %d".formatted(attack.name(), attack.type(), attack.owner(), attack.strength());
+        if (o instanceof Attack(PokemonKind type,int strength,String name,Pokemon owner)) {
+            return "Attack %s (%s) from %s, strength of %d".formatted(name, type, owner, strength);
         }
         return "Unknown";
     }
